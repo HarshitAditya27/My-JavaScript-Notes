a850be5adf5a18ebd02317e094e455a1aa5ed2af
Harshit <harshitaditya27@gmail.com>
2021-04-24 09:33:42 +0530

    Added files

---
+++ 1.js
@@ -0,0 +1,50 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log("Harshit");
+console.log(23);
+let firstName = "Harshit";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+/*
+const money = 0;
+if (money) {
+    console.log("Chill Bro");
+}
+else {
+    console.log("Bro you should earn money");
+}
+*/
+/*
+const age = 18;
+if (age === 18) alert('You just became an adult :D');
+*/
+//Mark :-  
+let Markmass = 78;
+let Markheight = 1.69;
+let BMIMark = Markmass / (Markheight ** 2);
+let Johnmass = 95;
+let Johnheight = 1.88;
+let BMIJohn = Johnmass / (Johnheight ** 2);
+if (BMIMark > BMIJohn) {
+    console.log(`Marks BMI(${BMIMark}) is higher then Johns BMI(${BMIJohn})`)
+}
+else {
+    console.log('Johns BMI ${BMIJohn} is higher then Marks BMI ${BMIMark}')
+}
+
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110)3;
+console.log(scoreDolphins, scoreKoalas);
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphines win the trophy');
+}
+else if (scoreDolphins < scoreKoalas) {
+    console.log('Koalas win the trophy');
+}
+else if (scoreDolphins === scoreKoalas) {
+    console.log('Both win the trophy');
+}
