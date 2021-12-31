ca7c0837f8874a6f02ed571363d58212b1f0a168
Harshit <harshitaditya27@gmail.com>
2021-12-31 23:43:59 +0530

    Added Section 3 notes

--- Notes/Section 3/script.js
+++ Notes/Section 3/script.js
@@ -6,7 +6,7 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('You are allowed to drive'); 
-*/
+
 
 //Functions 
 function logger() {
@@ -67,11 +67,37 @@ const retire = yearsUntilRetirement(2003);
 console.log(retire);
 
 
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
 
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+    return juice;
+}
 
+console.log(fruitProcessor(2, 3));
+*/
 
+const calcAge = function (birthYear) {
+    return 2021 - birthYear;
+}
 
+const yearsUntilRetirement = function (birthYear, name) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(`${name} will retire in ${retirement} years`);
+        return retirement;
 
+    } else {
+        return -1;
+        console.log(`${name} is already retired`);
+    }
+}
+console.log(yearsUntilRetirement(2003, 'Harshit'));
 
 
 
