4f8acd057c5d2134e17d7ee22c5dd14e99e2ddf2
Harshit Aditya <harshitaditya27@gmail.com>
2023-06-11 21:02:23 +0530

    Added scope notes

---
+++ Chai Aur Code/scope_1.js
@@ -0,0 +1,11 @@
+let a =300;
+//var c =300
+if (true){
+let a = 10; 
+const b = 20; 
+// c = 30;  
+console.log("Innner: ",a)
+}
+console.log(a); 
+//console.log(b); 
+//console.log(c);
---
+++ Chai Aur Code/scope_2.js
@@ -0,0 +1,38 @@
+/*
+function one(){
+    const username = 'harshit'; 
+    function two(){ 
+        const website = 'github'
+        console.log(username);
+    } 
+    //console.log(website); 
+    //two()
+}
+
+one(); 
+*/ 
+/*
+if(true){
+    const username = 'harshit' 
+    if(username === 'harshit'){
+        const website = 'github' 
+        console.log(username+website);
+    } 
+    //console.log(website)
+} 
+//console.log(username) 
+*/
+function addOne(num){
+    return num + 1;
+} 
+
+console.log(addOne(5)); 
+
+console.log(addTwo(2))
+
+const addTwo = function(num){
+    return num+2;
+} 
+
+
+
