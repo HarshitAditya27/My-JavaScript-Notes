6ab75a433ec98b620100eddaaf9b2d0cf075ef8a
Harshit Aditya <harshitaditya27@gmail.com>
2023-07-08 14:09:43 +0530

    Added datatypes notes

---
+++ Chai Aur Code/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3); //If using this in Node.js, not browser
+
+//Code readibility
+
+console.log(3 + 4);
+console.log("Harshit");
+
+let name = "Harshit";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol =>unique
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //Undefined
+
+//NOTE: Null is an object
