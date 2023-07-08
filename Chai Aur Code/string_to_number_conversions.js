40f405f478920503542c446637c700458e649ac3
Harshit Aditya <harshitaditya27@gmail.com>
2023-07-08 20:13:34 +0530

    Added comparison of datatypes notes

---
+++ Chai Aur Code/string_to_number_conversions.js
@@ -0,0 +1,38 @@
+// ---- Operations ----
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(3 / 2);
+// console.log(3 % 2);
+
+// const str1 = "Hello";
+// const str2 = " HarshitAditya27 !";
+// const str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(((3 + 4) * 5) % 3); //2
+
+//trick conversion
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3;
+
+//Dont do this
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
