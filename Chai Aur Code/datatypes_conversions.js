a7207d16f65eb6c60cc3984d4967f5732b07d989
Harshit Aditya <harshitaditya27@gmail.com>
2023-07-08 19:57:44 +0530

    Added datatypes conversions notes

---
+++ Chai Aur Code/datatypes_conversions.js
@@ -0,0 +1,26 @@
+// let score = "33aa";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(Number(null));
+// console.log(Number("Harshit"));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "";
+// let booleanIsLoggedInh = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedInh);
+
+// 1 => true
+// "" => false
+// "Harshit" => truw
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
